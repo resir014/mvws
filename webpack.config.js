ab9d669aed9258e906f402017f8dea7d2cb9a30c
Resi Respati <resir014@gmail.com>
2018-04-14 01:48:28 +0700

    fixed entry being incorrectly placed (whoops)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
+  // Normally an `entry` isn't required with Webpack 4, but
+  // we'll put this here just in case you want to change the
+  // filename of your entry point.
+  entry: './src/index.js',
   module: {
-    // Normally an `entry` isn't required with Webpack 4, but
-    // we'll put this here just in case you want to change the
-    // filename of your entry point.
-    entry: './src/index.tsx',
     rules: [
       // Standard Babel with `babel-preset-env` and `babel-preset-react`.
       // See `.babelrc` to view your loaded preset and/or change it.
